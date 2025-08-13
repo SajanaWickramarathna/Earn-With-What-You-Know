8de963a98d4c5090d6f87fe4417c404972b58363
Sajana Wickramarathna <sajanawickramarathna143@gmail.com>
2025-08-13 13:51:53 +0530

    profile

--- frontend/src/Routes/creatorDashboard.js
+++ frontend/src/Routes/creatorDashboard.js
@@ -2,22 +2,23 @@ import { Route, Routes } from "react-router-dom";
 import Sidebar from "../Creator/components/sidebar";
 
 import Dashboard from "../Creator/Pages/dashboard";
+import Profile from "../Creator/Pages/profile";
 
 export default function CreatorDashboard() {
   return (
-    <div className="bg-white w-1/6 min-h-screen">
+    <div className="flex min-h-screen">
       {/* Sidebar */}
       <div className="bg-white w-1/6 min-h-screen">
         <Sidebar />
       </div>
 
-      
-        {/* Page Content */}
-        <div className="flex-1 bg-gray-100 p-4">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-           </Routes> 
-        </div>
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col bg-gray-100 p-4">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
     </div>
   );
 }
